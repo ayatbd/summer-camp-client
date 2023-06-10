a6d4ba14a2d088eb7a995757c526a8501ad445d3
ayatbd <albehestione@gmail.com>
2023-06-10 12:44:43 +0600

    added animation and updated slider

--- src/pages/Home/Animation.jsx
+++ src/pages/Home/Animation.jsx
@@ -1,11 +1,18 @@
+import { Fade, Slide } from "react-awesome-reveal";
 
 
 const Animation = () => {
     return (
-        <div>
-            
-        </div>
-    );
+        <div className="text-center bg-emerald-300 py-28 my-20">
+      <Slide>
+        <h1 className="text-4xl font-bold mb-5">New language, new <br /> opportunities, new you</h1>
+      </Slide>
+      <Fade delay={1e3} cascade damping={1e-1}>
+      Get access to compact lessons from the experts and connect
+      with a community of native speakers to help you master words faster.
+      </Fade>
+    </div>
+      );
 };
 
 export default Animation;
--- src/pages/Home/Slider.jsx
+++ src/pages/Home/Slider.jsx
@@ -1,10 +1,13 @@
 import img1 from "../../assets/images/img2.jpg";
+import img3 from "../../assets/images/img3.png";
+import img4 from "../../assets/images/img4.jpg";
+import img5 from "../../assets/images/img5.jpeg";
 
 const Slider = () => {
   return (
     <div className="carousel h-[28rem] w-full my-20">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={img1} className="w-full opacity-60" alt="Slide 1" />
+        <img src={img4} className="w-full" alt="Slide 1" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">
             ❮
@@ -20,7 +23,7 @@ const Slider = () => {
         </div>
       </div>
       <div id="slide2" className="carousel-item relative w-full">
-        <img src={img1} className="w-full opacity-70" alt="Slide 2" />
+        <img src={img3} className="w-full" alt="Slide 2" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -36,7 +39,7 @@ const Slider = () => {
         </div>
       </div>
       <div id="slide3" className="carousel-item relative w-full">
-        <img src={img1} className="w-full opacity-70" alt="Slide 3" />
+        <img src={img1} className="w-full" alt="Slide 3" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -52,7 +55,7 @@ const Slider = () => {
         </div>
       </div>
       <div id="slide4" className="carousel-item relative w-full">
-        <img src={img1} className="w-full opacity-70" alt="Slide 4" />
+        <img src={img5} className="w-full" alt="Slide 4" />
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">
             ❮
